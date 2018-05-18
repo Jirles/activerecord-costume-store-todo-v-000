30f016bf7dcf56218fb14374ded80ce120476bee
Emily Jirles <emily.jirles@gmail.com>
2018-05-18 00:07:29 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -11,6 +11,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.datetime :opening_date
       t.datetime :closing_date
       t.string :long_description
+      
+      t.timestamps
     end 
   end 
   
